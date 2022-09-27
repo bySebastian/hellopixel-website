9eee0dcc4938e7e1289902e41273b83fefa87ae0
Sebastian Vyaene <sebastian@marlon.be>
2022-09-27 08:34:40 +0200

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,34 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import { mdsvex } from "mdsvex";
+import preprocess from "svelte-preprocess";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import autoprefixer from "autoprefixer";
+import mediaVariables from "postcss-media-variables";
+
+export const prerender = true;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+
+	extensions: [".svelte", ".md"],
+
+	preprocess: [
+		preprocess({
+			postcss: {
+				plugins: [autoprefixer, mediaVariables]
+			},
+		}),
+		mdsvex({
+			extensions: [".md"],
+			rehypePlugins: [
+				rehypeSlug,
+				rehypeAutolinkHeadings,
+			]
+		}),
+	],
 
 	kit: {
 		adapter: adapter()
