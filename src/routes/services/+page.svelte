97761172d38abfe3e6dee1c022b2eac59a6c7dcb
Sebastian Vyaene <sebastian@marlon.be>
2022-09-30 21:58:48 +0200

    add header styling to services page

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -9,43 +9,43 @@
 
 <section>
     <article>
-        <h3>Prototypes</h3>
+        <h3 class="header header--green">Prototypes</h3>
         <p>Voor we starten met de vormgeving van de app, webshop of website werken we tijdens de analysefase een user experience design uit. Dat is een prototype waarin we **alle functionaliteiten visueel presenteren, vanuit het mobile-first principe.**</p>
         <p>In het prototype stellen we de **gebruiker centraal** en is er nog geen sprake van design met kleuren en logo’s. Zo kan het gebruiksgemak gecontroleerd worden: gebeuren alle acties intuïtief en kan de gebruiker navigeren zoals verwacht?</p>    </article>
     <article>
-        <h3>Design</h3>
+        <h3 class="header header-lilac">Design</h3>
     </article>
     <article>
-        <h3>Strategy</h3>
+        <h3 class="header header-yellow">Strategy</h3>
         <p>Vandaag zijn de verwachtingen hoog gespannen op vlak van technologie en ervaringen. Mensen verwachten kwaliteit, maar ook een **mooi vormgegeven app die efficiënt werkt**. De inhoud moet waardevol en relevant zijn, spreken tot de verbeelding of net heel doelgericht de gebruiker helpen zonder omwegen. Een evenwichtige mix van deze elementen is cruciaal.</p>
         <p>Daarom werken we samen een **onderbouwde digitale strategie** uit waarin we het doel en de platform definiëren. Is een mobile app wat je nodig hebt of is een responsive website of webapplicatie een betere oplossing? Wij geven je advies en reiken de **meest rendabele oplossingen** aan.</p>
     </article>
     <article>
-        <h3>User Research</h3>
+        <h3 class="header header--orange">User Research</h3>
     </article>
     <article>
-        <h3>Usability testing</h3>
+        <h3 class="header header--blue">Usability testing</h3>
     </article>
     <article>
-        <h3>Accessibility testing</h3>
+        <h3 class="header header--lime">Accessibility testing</h3>
     </article>
     <article>
-        <h3>Websites</h3>
+        <h3 class="header header--red-pink">Websites</h3>
         <p>Een website moet gevonden worden en de aandacht trekken. Een uniek design is hierbij cruciaal. In combinatie met een goede structuur, duidelijke copy en logische opbouwen zorgen wij ervoor dat jouw online aanwezigheid opgemerkt wordt.</p>
     </article>
     <article>
-        <h3>Webshops</h3>
+        <h3 class="header header--pink">Webshops</h3>
         <p>Een succesvolle webshop is meer dan een goed aanbod. Wij staan in voor development en design, maar helpen je ook bij het uitwerken van je strategie.</p>
         <p>Zo zorgen we ervoor dat je webshop op verschillende vlakken doordacht is opgebouwd: gebruiksvriendelijk, visueel aantrekkelijk, vindbaar en in lijn met je business logica.</p>
     </article>
     <article>
-        <h3>Web Applications</h3>
+        <h3 class="header header--beige">Web Applications</h3>
         <p>Vereenvoudig processen, laat klanten zelf hun product samenstellen of plan al je evenementen eenvoudig in. Webapplicaties maken het leven van de gebruiker gemakkelijker en zorgen voor tijdwinst.</p>
         <p>Een webapplicatie is een tool die je kan gebruiken via je browser en waar heel wat achterliggende dataverwerking gebeurt. Ze bestaan in allerlei vormen.</p>
         <p>Denk maar aan aant: productconfigurator, planningstool, beheersplatform...</p>
     </article>
     <article>
-        <h3>Marketing</h3>
+        <h3 class="header header--stormy-blue">Marketing</h3>
         <p>Online marketing is voor ons meer dan advertising. Het is onlosmakelijk met de ontwikkeling van je webproject. Zo zijn er heel wat marketingtechnische zaken die mee bepalen of het een succes wordt of niet. Maar geen nood, we helpen je van bij het begin om op alle vlakken te scoren.</p>
         <p>Ben je al een tijdje aanwezig op het internet? Dan starten we met enkele uitvoerige audits. Hoe goed scoor je op SEO-vlak, zowel technisch als inhoudelijk? Wat werkt er op je huidige website of shop en wat niet? Hoe goed doe je SEA het? Aan de hand van enkele audits brengen we meteen in kaart wat er beter kan of waar er groeimarge zit. De ideale basis om om je nieuwe marketingstrategie op te bouwen.</p>
     </article>  
